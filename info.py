6ebcf9bd39623bd6f2d2248e4d9248b46c6f64e5
Akashnm10 <akash.n.m1040@gmail.com>
2024-11-24 21:25:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '24320761'))
+API_ID = int(environ.get('API_ID', ' 24320761 '))
 API_HASH = environ.get('API_HASH', '99d31f6fc8f6dca1b61d7b81e8ba88bb')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
